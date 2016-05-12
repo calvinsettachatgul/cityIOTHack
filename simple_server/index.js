9e7329a5268aafa1fd47dabb65cfd627bca27340
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-05-12 10:29:33 -0700

    add simple server logic and post data

---
+++ simple_server/index.js
@@ -0,0 +1,45 @@
+http = require('http');
+fs = require('fs');
+qs = require('querystring');
+var bodyParser = require('body-parser')
+
+server = http.createServer( function(req, res) {
+
+    console.dir(req.param);
+
+    if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function () {
+            console.log("Body: " + body);
+            var post = qs.parse(body);
+            console.log(post['data']);
+        });
+
+
+
+        // var response_string = req.body;
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        console.log("this is the response");
+        res.end("thats it!");
+    }
+    else
+    {
+        console.log("GET");
+        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+        var html = fs.readFileSync('index.html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+    }
+
+});
+
+port = 3000;
+host = '127.0.0.1';
+server.listen(port, host);
+console.log('Listening at http://' + host + ':' + port);
